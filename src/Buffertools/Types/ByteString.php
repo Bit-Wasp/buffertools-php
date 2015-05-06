24e27a7753d6e6286a6a5c82d88e5e5ae666d94e
Thomas Kerin <thomas@bitwasp.co>
2015-05-06 18:10:29 +0100

    Add ByteString serializer (fixed length)

---
+++ src/Buffertools/Types/ByteString.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace BitWasp\Buffertools\Types;
+
+
+use BitWasp\Buffertools\Buffer;
+use BitWasp\Buffertools\ByteOrder;
+use BitWasp\Buffertools\Parser;
+use Mdanter\Ecc\Math\MathAdapterInterface;
+
+class ByteString extends AbstractType
+{
+    /**
+     * @var int|string
+     */
+    private $length;
+
+    /**
+     * @param MathAdapterInterface $math
+     * @param int|string $length
+     * @param int|string $byteOrder
+     */
+    public function __construct(MathAdapterInterface $math, $length, $byteOrder = ByteOrder::BE)
+    {
+        $this->length = $length;
+        parent::__construct($math, $byteOrder);
+    }
+
+    /**
+     * @param Buffer $string
+     * @return string
+     */
+    public function writeBits(Buffer $string)
+    {
+        $bits = str_pad(
+            $this->getMath()->baseConvert($string->getHex(), 16, 2),
+            $this->length * 8,
+            '0',
+            STR_PAD_LEFT
+        );
+
+        return $bits;
+    }
+
+    /**
+     * @param $string
+     * @return string
+     * @throws \Exception
+     */
+    public function write($string)
+    {
+        if (false === $string instanceof Buffer) {
+            throw new \InvalidArgumentException('FixedLengthString::write() must be passed a Buffer');
+        }
+
+        $bits = $this->isBigEndian()
+            ? $this->writeBits($string)
+            : $this->flipBits($this->writeBits($string));
+
+        $hex = str_pad(
+            $this->getMath()->baseConvert($bits, 2, 16),
+            $this->length,
+            '0',
+            STR_PAD_LEFT
+        );
+
+        return pack("H*", $hex);
+    }
+
+    /**
+     * @param Buffer $buffer
+     * @return string
+     */
+    public function readBits(Buffer $buffer)
+    {
+        return str_pad(
+            $this->getMath()->baseConvert($buffer->getHex(), 16, 2),
+            $this->length * 8,
+            '0',
+            STR_PAD_LEFT
+        );
+    }
+
+    /**
+     * @param Parser $parser
+     * @return Buffer
+     * @throws \BitWasp\Buffertools\Exceptions\ParserOutOfRange
+     */
+    public function read(Parser & $parser)
+    {
+        $bits = $this->readBits($parser->readBytes($this->length));
+        if (!$this->isBigEndian()) {
+            $bits = $this->flipBits($bits);
+        }
+
+        return Buffer::hex($this->getMath()->baseConvert($bits, 16, 2), $this->length);
+    }
+}
