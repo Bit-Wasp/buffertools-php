43c094ab413aa97c7af6e3a72ce41936b7ea0217
Thomas Kerin <thomas@bitwasp.co>
2015-05-23 21:24:17 +0100

    Add test for bytestring LE

--- tests/Types/ByteStringTest.php
+++ tests/Types/ByteStringTest.php
@@ -1,16 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: thomas
- * Date: 23/05/15
- * Time: 17:26
- */
 
 namespace BitWasp\Buffertools\Tests\Types;
 
 
 use BitWasp\Buffertools\Buffer;
-use BitWasp\Buffertools\BuffertoolsTest;
+use BitWasp\Buffertools\Buffertools;
+use BitWasp\Buffertools\ByteOrder;
 use BitWasp\Buffertools\Parser;
 use BitWasp\Buffertools\Tests\BinaryTest;
 use BitWasp\Buffertools\Types\ByteString;
@@ -20,6 +15,7 @@ class ByteStringTest extends BinaryTest
 {
     public function getVectors()
     {
+        echo 'called';
         $math = MathAdapterFactory::getAdapter();
         return [
             [$math, 1, '04'],
@@ -39,6 +35,23 @@ class ByteStringTest extends BinaryTest
         $out = $t->write($buffer);
 
         $this->assertEquals(pack("H*", $string), $out);
+
+        $parser = new Parser(new Buffer($out));
+        $this->assertEquals($string, $t->read($parser)->getHex());
+    }
+
+    /**
+     * @dataProvider getVectors
+     */
+    public function testByteStringLe($math, $size, $string)
+    {
+        $buffer = Buffer::hex($string, $size);
+
+        $t = new ByteString($math, $size, ByteOrder::LE);
+        $out = $t->write($buffer);
+
+        $eFlipped = Buffertools::flipBytes(pack("H*", $string));
+        $this->assertEquals($eFlipped, $out);
 
         $parser = new Parser(new Buffer($out));
         $this->assertEquals($string, $t->read($parser)->getHex());
