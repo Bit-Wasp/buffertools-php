4fc2b8cd7a759079cd998d262b419e2a8ac78ebc
Thomas Kerin <me@thomaskerin.io>
2016-12-22 02:38:16 +0100

    sanity checks

--- src/Buffertools/Buffer.php
+++ src/Buffertools/Buffer.php
@@ -67,7 +67,7 @@ class Buffer implements BufferInterface
     public static function hex($hexString = '', $byteSize = null, GmpMathInterface $math = null)
     {
         if (strlen($hexString) > 0 && !ctype_xdigit($hexString)) {
-            throw new \InvalidArgumentException('BufferHex: non-hex character passed: ' . $hexString);
+            throw new \InvalidArgumentException('Buffer::hex: non-hex character passed');
         }
 
         $math = $math ?: EccFactory::getAdapter();
@@ -83,6 +83,10 @@ class Buffer implements BufferInterface
      */
     public static function int($integer, $byteSize = null, GmpMathInterface $math = null)
     {
+        if ($integer < 0) {
+            throw new \InvalidArgumentException('Negative integers not supported by Buffer::int. This could be an application error, or you should be using templates.');
+        }
+
         $math = $math ?: EccFactory::getAdapter();
         $binary = pack("H*", $math->decHex($integer));
         return new self($binary, $byteSize, $math);
@@ -109,6 +113,10 @@ class Buffer implements BufferInterface
         }
 
         $string = substr($this->getBinary(), $start, $end);
+        if (!is_string($string)) {
+            throw new \RuntimeException('Failed to slice string of with requested start/end');
+        }
+
         $length = strlen($string);
         return new self($string, $length, $this->math);
     }
