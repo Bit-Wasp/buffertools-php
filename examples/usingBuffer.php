83e45274618f5fd414058c815a94868e647c00f8
Thomas Kerin <thomas.kerin@bitmaintech.com>
2017-12-26 22:18:27 +0100

    Install phpstan for tests/ci

--- examples/usingBuffer.php
+++ examples/usingBuffer.php
@@ -3,8 +3,6 @@
 require "../vendor/autoload.php";
 
 use BitWasp\Buffertools\Buffer;
-use BitWasp\Buffertools\BufferHex;
-use BitWasp\Buffertools\BufferInt;
 
 // Binary data and ASCII can be passed directly to a Buffer
 $binary = new Buffer('hello world');
@@ -12,12 +10,12 @@ echo $binary->getBinary() . PHP_EOL;
 echo $binary->getHex() . PHP_EOL;
 
 // BufferHex and BufferInt convert data to binary
-$hex = new BufferHex('68656c6c6f20776f726c64');
+$hex = Buffer::hex('68656c6c6f20776f726c64');
 echo $binary->getBinary() . PHP_EOL;
 echo $hex->getHex() . PHP_EOL;
 
 // All Buffers expose getBinary(), getInt(), getHex()
-$int = new BufferInt(65);
+$int = Buffer::int(65);
 echo $int->getBinary() . PHP_EOL;
 echo $int->getInt() . PHP_EOL;
 echo $int->getHex() . PHP_EOL;
