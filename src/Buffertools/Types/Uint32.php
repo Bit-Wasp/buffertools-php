60a4779a71b0230d6ee1219a093b60ee46b93bf3
Thomas Kerin <thomas.kerin@bitmaintech.com>
2018-11-11 14:24:31 +0100

    Uint32: use pack, much faster

--- src/Buffertools/Types/Uint32.php
+++ src/Buffertools/Types/Uint32.php
@@ -2,8 +2,13 @@
 
 namespace BitWasp\Buffertools\Types;
 
+use BitWasp\Buffertools\Parser;
+
 class Uint32 extends AbstractUint
 {
+    private $formatBE = "N";
+    private $formatLE = "V";
+
     /**
      * {@inheritdoc}
      * @see \BitWasp\Buffertools\Types\TypeInterface::getBitSize()
@@ -11,5 +16,30 @@ class Uint32 extends AbstractUint
     public function getBitSize()
     {
         return 32;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @see \BitWasp\Buffertools\Types\TypeInterface::write()
+     */
+    public function write($integer)
+    {
+        if ($this->isBigEndian()) {
+            return pack($this->formatBE, $integer);
+        }
+        return pack($this->formatLE, $integer);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @see \BitWasp\Buffertools\Types\TypeInterface::read()
+     */
+    public function read(Parser $parser)
+    {
+        $bytes = $parser->readBytes(4);
+        if ($this->isBigEndian()) {
+            return unpack($this->formatBE, $bytes->getBinary())[1];
+        }
+        return unpack($this->formatLE, $bytes->getBinary())[1];
     }
 }
